7c72e860550e59e74e1b3cca907e53a5e4507d93
classyp <jeanphilip@mecom>
2018-02-01 19:04:14 -0700

    bamazon table working with questions not in the right order

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,9 @@
 var inquirer = require('inquirer');
 var mysql = require('mysql');
-require("console.table")
+//require("console.table")
+const cTable = require('console.table');
 var Table = require('cli-table');
-require("console.table")
+//require("console.table")
 
 
 // Define the MySQL connection parameters
@@ -13,3 +14,43 @@ var connection = mysql.createConnection({
     password: '',
     database: 'bamazon_db'
 });
+
+connection.connect((err) => {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+    console.log('connected as id ' + connection.threadId);
+  });
+
+getProducts();
+// Created a series of questions
+inquirer.prompt([
+  {
+    type: "input",
+    name: "shopItem",
+    message: "What item woul you like to buy???"
+  },
+  {
+    type: "input",
+    name: "amount",
+    message: "How many would you like?"
+  }
+
+]).then((answers) => {
+    console.log(answers);
+    console.log(answers.amount);   
+});
+
+// Gets all the products from the "products" table and logs them to the console as a table
+function getProducts() {
+    // query mysql table
+    connection.query('SELECT * FROM products', function (error, results) {
+        if (error) console.log(error);
+        // connected!
+        console.table(results);
+        //console.log(results);
+      });
+    // log resuts to the console
+    
+};
