e54a6151af5fe7c0345e6507fa609159af9f6fe1
classyp <jeanphilip@mecom>
2018-05-26 14:56:26 -0700

    updating sql db

--- bamazon.sql
+++ bamazon.sql
@@ -1,15 +1,27 @@
-DROP DATABASE IF EXISTS bamazon_DB;
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
 
-CREATE DATABASE bamazon_DB;
+USE bamazon;
 
-USE bamazon_DB;
+CREATE TABLE products(
+  item_id INT AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(45) NOT NULL,
+  department_name VARCHAR(45) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock_quantity INT(10) NOT NULL,
+  primary key(item_id)
+);
 
-     
-create table products (
-      item_id INT NOT NULL AUTO_INCREMENT,
-	  Product_name VARCHAR(45) NULL,
-	  Department_name VARCHAR(45) NULL,
-	  Price DECIMAL(10,2) NULL,
-	  Stock_quantity INT(10),
-	  PRIMARY KEY (item_id)
-      );
+SELECT * FROM products;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Uncharted 4", "Video Games", 49.95, 150),
+  ("DOOM", "Video Games", 59.99, 200),
+  ("Crate of Spam", "Food and Drink", 24.50, 50),
+  ("Cool Shades", "Apparel", 75.00, 5),
+  ("Worn Denim Jeans", "Apparel", 54.25, 35),
+  ("Survival Towel", "Necessities", 42.42, 42),
+  ("Bill and Ted's Excellent Adventure", "Films", 15.00, 25),
+  ("Mad Max: Fury Road", "Films", 25.50, 57),
+  ("Monopoly", "Board Games", 30.50, 35),
+  ("Yahtzee", "Board Games", 19.95, 23);
